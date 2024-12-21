010b5e623c552d34b30ddeee3098180b63ab1f09
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2024-12-21 01:42:08 -0600

    Added bflat build command to TypeSwitch

---
+++ src/Tests/Behavioral/TypeSwitch/build.cmd
@@ -0,0 +1,2 @@
+mkdir bin\win-x64
+bflat build .\TypeSwitch.cs ..\..\..\gocore\GlobalUsings.cs -o:bin\win-x64\TypeSwitch.exe --os:windows --arch:x64 --stdlib:DotNet --no-globalization -r:..\..\..\gocore\golib\bin\Debug\net8.0\golib.dll -r:..\..\..\gocore\fmt\bin\Debug\net8.0\fmt_package.dll 
