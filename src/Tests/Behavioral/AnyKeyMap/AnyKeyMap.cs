aaafd7090a451e72305ad886ae2d06461568c130
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2025-02-25 20:58:48 -0600

    Updated maps with empty interface (any) type to work with non-U8 strings

--- src/Tests/Behavioral/AnyKeyMap/AnyKeyMap.cs
+++ src/Tests/Behavioral/AnyKeyMap/AnyKeyMap.cs
@@ -7,11 +7,10 @@ partial class main_package {
 private static void Main() {
     map<any, @string> tbl = default!;
     tbl = new map<any, @string>();
-    // Ensure object-based map key parameters as strings are not u8
-    tbl[@string("12"u8)] = "1"u8;
+    tbl["12"] = "1"u8;
     tbl[12] = "2"u8;
     tbl[12.0F] = "3"u8;
-    fmt.Println(tbl["12"u8]);
+    fmt.Println(tbl["12"]);
     fmt.Println(tbl[12]);
     fmt.Println(tbl[12.0F]);
 }
--- src/go2cs/convIndexExpr.go
+++ src/go2cs/convIndexExpr.go
@@ -3,8 +3,23 @@ package main
 import (
 	"fmt"
 	"go/ast"
+	"go/types"
 )
 
 func (v *Visitor) convIndexExpr(indexExpr *ast.IndexExpr) string {
-	return fmt.Sprintf("%s[%s]", v.convExpr(indexExpr.X, nil), v.convExpr(indexExpr.Index, nil))
+	var contexts []ExprContext
+
+	if typeAndVal, ok := v.info.Types[indexExpr.X]; ok {
+		// Check if the type is a map and its key is an empty interface
+		if mapType, isMap := typeAndVal.Type.(*types.Map); isMap {
+			// Check if the key type is an empty interface
+			if types.Identical(mapType.Key(), types.NewInterfaceType(nil, nil)) {
+				context := DefaultBasicLitContext()
+				context.u8StringOK = false
+				contexts = []ExprContext{context}
+			}
+		}
+	}
+
+	return fmt.Sprintf("%s[%s]", v.convExpr(indexExpr.X, nil), v.convExpr(indexExpr.Index, contexts))
 }
