106e0e3ac2c9c5dd31bcdbea4d0084ed9722b12a
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2025-03-30 12:05:28 -0500

    Fixed code left alignment

--- src/core/unsafe/unsafe.cs
+++ src/core/unsafe/unsafe.cs
@@ -250,23 +250,23 @@ public static uintptr Offsetof(Type structType, string fieldName) {
     return (uintptr)Marshal.OffsetOf(structType, fieldName);
 }
 
-    // Alignof takes an expression x of any type and returns the required alignment
-    // of a hypothetical variable v as if v was declared via var v = x.
-    // It is the largest value m such that the address of v is always zero mod m.
-    // It is the same as the value returned by [reflect.TypeOf](x).Align().
-    // As a special case, if a variable s is of struct type and f is a field
-    // within that struct, then Alignof(s.f) will return the required alignment
-    // of a field of that type within a struct. This case is the same as the
-    // value returned by [reflect.TypeOf](s.f).FieldAlign().
-    // The return value of Alignof is a Go constant if the type of the argument
-    // does not have variable size.
-    // (See the description of [Sizeof] for a definition of variable sized types.)
-    // go2cs conversion converts:
-    // `unsafe.Alignof(x)` to
-    // `@unsafe.Alignof(x.GetType())` and
-    // `unsafe.Alignof(s.f)` to
-    // `@unsafe.Alignof(s.GetType(), "f")`
-    public static uintptr Alignof(Type type, string? fieldName = null) {
+// Alignof takes an expression x of any type and returns the required alignment
+// of a hypothetical variable v as if v was declared via var v = x.
+// It is the largest value m such that the address of v is always zero mod m.
+// It is the same as the value returned by [reflect.TypeOf](x).Align().
+// As a special case, if a variable s is of struct type and f is a field
+// within that struct, then Alignof(s.f) will return the required alignment
+// of a field of that type within a struct. This case is the same as the
+// value returned by [reflect.TypeOf](s.f).FieldAlign().
+// The return value of Alignof is a Go constant if the type of the argument
+// does not have variable size.
+// (See the description of [Sizeof] for a definition of variable sized types.)
+// go2cs conversion converts:
+// `unsafe.Alignof(x)` to
+// `@unsafe.Alignof(x.GetType())` and
+// `unsafe.Alignof(s.f)` to
+// `@unsafe.Alignof(s.GetType(), "f")`
+public static uintptr Alignof(Type type, string? fieldName = null) {
     // Handle the special case for struct fields
     if (fieldName is not null && type is { IsValueType: true, IsPrimitive: false })
     {
